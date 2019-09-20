f8dff2dc405e0877102e37385ce4a6b04c9e404d
Joshua Swick <joshuaswickirl@gmail.com>
2019-09-20 15:00:54 -0600

    Refactor to use \b\w to get first letter

--- acronym/acronym.rb
+++ acronym/acronym.rb
@@ -1,12 +1,5 @@
 class Acronym
-
   def self.abbreviate(phrase)
-
-    phrase
-      .scan(/\w+/)                 # scan for words
-      .map{ |letter| letter[0] }   # get first letters
-      .join                        # join string
-      .upcase                      # as uppercase
-
+    phrase.scan(/\b\w/).join.upcase
   end
 end
